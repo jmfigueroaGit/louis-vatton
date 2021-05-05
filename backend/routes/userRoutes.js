d52f934dca86f390e27bd5735e929fc247ea7dd1
jmfigueroaGit <jheremiah.figueroa@tup.edu.ph>
2021-05-05 15:04:31 +0800

    Login Register Frontend/Backend Done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import { authUser, registerUser } from '../controllers/userController.js';
 
-router.route('/register').post(registerUser);
+router.post('/register', registerUser);
 router.post('/login', authUser);
 
 export default router;
---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <Route path='/login' component={LoginScreen} />
+                    <Route path='/register' component={RegisterScreen} />
+                </Container>
+            </main>
+        </Router>
+    );
+};
+
+export default App;
