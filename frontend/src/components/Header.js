af37fa8c2e3046c4f27b9e802b67b7575bfff724
Tenpai10 <pransarbey@gmail.com>
2021-06-16 14:28:45 +0800

    fixed issue add to cart

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,77 +1,77 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import { logout } from '../actions/userActions';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { logout } from "../actions/userActions";
+import { Fragment } from "react";
 
 const Header = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const userLogin = useSelector((state) => state.userLogin);
-    const { userInfo } = userLogin;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const logoutHandler = () => {
-        dispatch(logout());
-    };
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
 
-    return (
-        <header>
-            <Navbar
-                className='header'
-                variant='dark'
-                expand='lg'
-                collapseOnSelect
-            >
-                <Container>
-                    <LinkContainer to='/'>
-                        <Navbar.Brand>Louis Vatton</Navbar.Brand>
+  return (
+    <header>
+      <Navbar className="header" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>ToyStorey</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {userInfo && !userInfo.isAdmin && (
+                <>
+                  <LinkContainer to="/cart">
+                    <Nav.Link className="text-color">
+                      <i className="fas fa-shopping-cart text-color"></i>
+                      <span className="text-color"> Cart</span>
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+              {userInfo ? (
+                <div className="flex-row">
+                  <NavDropdown
+                    title={userInfo.name}
+                    id="username"
+                    style={{ color: "white" }}
+                  >
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </div>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link className="text-color">
+                    <i className="fas fa-user"></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <>
+                  <NavDropdown title="Admin" id="adminmenu">
+                    <LinkContainer to="/admin/userlist">
+                      <NavDropdown.Item>Users</NavDropdown.Item>
                     </LinkContainer>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='ml-auto'>
-                            <LinkContainer to='/cart'>
-                                <Nav.Link className='text-color'>
-                                    <i className='fas fa-shopping-cart text-color'></i>
-                                    <span className='text-color'> Cart</span>
-                                </Nav.Link>
-                            </LinkContainer>
-                            {userInfo ? (
-                                <NavDropdown
-                                    title={userInfo.name}
-                                    id='username'
-                                    style={{ color: 'white' }}
-                                >
-                                    <NavDropdown.Item onClick={logoutHandler}>
-                                        Logout
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            ) : (
-                                <LinkContainer to='/login'>
-                                    <Nav.Link className='text-color'>
-                                        <i className='fas fa-user'></i> Sign In
-                                    </Nav.Link>
-                                </LinkContainer>
-                            )}
-                            {userInfo && userInfo.isAdmin && (
-                                <NavDropdown title='Admin' id='adminmenu'>
-                                    <LinkContainer to='/admin/userlist'>
-                                        <NavDropdown.Item>
-                                            Users
-                                        </NavDropdown.Item>
-                                    </LinkContainer>
-                                    <LinkContainer to='/admin/productlist'>
-                                        <NavDropdown.Item>
-                                            Products
-                                        </NavDropdown.Item>
-                                    </LinkContainer>
-                                </NavDropdown>
-                            )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    );
+                    <LinkContainer to="/admin/productlist">
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
